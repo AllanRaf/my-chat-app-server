120d508da1913e88d31a66c151e9e379d82a3201
AllanRaf <rafale.uk@googlemail.com>
2020-07-28 04:49:35 +0200

    Remove db sync

--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { User } = require("../models/user");
+const { User } = require("../models/index");
 const bcrypt = require("bcrypt");
 const router = new Router();
 
