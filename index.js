a5e7b9d5a0146fcb404e83f210fe0797af60757b
AllanRaf <rafale.uk@googlemail.com>
2020-07-28 03:59:43 +0200

    re add all routers

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-//const userRouter = require("./routers/user");
-//const chatRoomRouter = require("./routers/chatroom");
+const userRouter = require("./routers/user");
+const chatRoomRouter = require("./routers/chatroom");
 const authRouter = require("./routers/auth");
 //const http = require("http");
 
@@ -14,14 +14,16 @@ console.log("authrouter", authRouter);
 /* const server = http.Server(app);
 const io = require("socket.io")(server); */
 
-app.use(cors()).use(jsonParser);
-/*   .use(function (request, response, next) {
+app
+  .use(cors())
+  .use(jsonParser)
+  /*   .use(function (request, response, next) {
     request.io = io;
     next();
   }) */
-// .use(authRouter);
-// .use(userRouter);
-//.use(chatRoomRouter);
+  .use(authRouter)
+  .use(userRouter)
+  .use(chatRoomRouter);
 
 /* io.use(async (socket, next) => {
   try {
