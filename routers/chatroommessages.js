07414584e6d008c2209079b0cebb5d51e64ce38a
AllanRaf <rafale.uk@googlemail.com>
2020-11-08 17:37:08 +0100

    feat: send new messages back over socket

--- routers/chatroommessages.js
+++ routers/chatroommessages.js
@@ -1,24 +1,33 @@
 const { Router } = require("express");
-const { ChatroomMessages } = require("../models");
+const { ChatroomMessages, User } = require("../models");
 const auth = require("../middleware/auth");
 const router = new Router();
 
 router.get("/messages/:roomId", auth, async (request, response) => {
   console.log("fetching messages", request.params.roomId);
-  /*   const messages = await ChatroomMessages.findAll();
-    response.status(201).json(messages); */
+
   response.status(201).json({ roomId: request.params.roomId });
 });
 
 router.post("/messages/:roomId", auth, async (request, response) => {
-  console.log("posting a message", request.params.roomId);
-
   const chatRoomMessage = await ChatroomMessages.create({
     roomId: request.params.roomId,
     userId: request.userId,
-    message: "hello",
+    message: request.body.messageToSend,
   });
-  console.log("the new message", chatRoomMessage);
+  //find the username of the person who sent the message
+
+  const user = await User.findOne({ where: { id: request.userId } });
+  console.log("user found is", user);
+
+  let event = {};
+  event.roomName = request.body.roomName;
+  event.username = user.dataValues.username;
+  event.message = request.body.messageToSend;
+
+  request.io.emit(request.body.roomName, event);
+
+  response.status(201).json({ chatRoomMessage });
 });
 
 module.exports = router;
